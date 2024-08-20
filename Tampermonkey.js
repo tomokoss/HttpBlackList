23fe0b30789f936e661fe5b21f7817f748123c9f
子谭一 <t1748617439@163.com>
2024-08-21 00:16:07 +0800

    update

--- Tampermonkey.js
+++ Tampermonkey.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         网站屏蔽示例
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  尝试在访问被屏蔽的网站时显示警告并关闭页面
+// @version      1.3
+// @description  访问被屏蔽的网站时显示警告并重定向
 // @author       tanz
 // @match        *://*/*
+// @connect      *
 // @grant        none
-// @connect      yourusername.github.io
 // ==/UserScript==
 
 (function() {
@@ -15,34 +15,41 @@
     // 动态加载远程JavaScript文件
     function loadScript(url, callback) {
         var script = document.createElement('script');
+        script.src = url;
         script.onload = function() {
-            if (typeof callback === 'function') {
-                callback();
+            if (window.blockedUrls && Array.isArray(window.blockedUrls)) {
+                callback(window.blockedUrls);
+            } else {
+                console.error('blockedUrls not defined in the loaded script.');
             }
         };
-        script.src = url;
+        script.onerror = function() {
+            console.error('Error loading the script:', url);
+        };
         document.head.appendChild(script);
     }
 
-    // 加载远程屏蔽列表
+    // 远程屏蔽列表URL
     var remoteListUrl = 'https://qipantanyi.github.io/HttpBlackList/blocklist.js';
-    loadScript(remoteListUrl, function() {
-        // 确保远程列表已加载并命名为window.blockedUrls
-        if (window.blockedUrls) {
-            checkAndBlockSites(window.blockedUrls);
-        } else {
-            console.error('blockedUrls not defined in the loaded script.');
-        }
+    loadScript(remoteListUrl, function(blockedUrls) {
+        // 使用获取到的blockedUrls进行检查
+        checkAndBlockSites(blockedUrls);
     });
 
     // 检查并屏蔽网站
     function checkAndBlockSites(blockedUrls) {
         for (var i = 0; i < blockedUrls.length; i++) {
-            if (location.href.startsWith(blockedUrls[i])) {
+            var blockedUrl = new URL(blockedUrls[i]);
+            if (location.host === blockedUrl.host && location.pathname.startsWith(blockedUrl.pathname)) {
                 alert('访问的网站被屏蔽！');
-                window.close(); // 关闭当前页面
+                redirectToBlockedPage(); // 重定向到被屏蔽的页面
                 return;
             }
         }
+    }
+
+    // 重定向到被屏蔽的页面
+    function redirectToBlockedPage() {
+        window.location.href = 'https://qipantanyi.github.io/HttpBlackList/access-denied.html';
     }
 })();
